e441bf937e48b019dc320eb76197a1249523b373
Andreas Disman <anddi182@student.liu.se>
2022-03-27 13:38:46 +0200

    Testar funktionalitet hos en display, endast drivare, ingen latch

--- arduino/BuzzWireGame/BuzzWireGame.ino
+++ arduino/BuzzWireGame/BuzzWireGame.ino
@@ -1,6 +1,48 @@
 
+// struct seven_seg {
+//     int address;
+//     int value;
+// };
+
+// void seven_seg_write(struct* seven_seg);
+
+#define LT_N    2
+#define A       3
+#define B       4
+#define C       5
+#define D       6
 void setup() {
+    pinMode(LT_N, OUTPUT);
+    pinMode(A, OUTPUT);
+    pinMode(B, OUTPUT);
+    pinMode(C, OUTPUT);
+    pinMode(D, OUTPUT);
+
+    digitalWrite(LT_N, HIGH);
+
+    Serial.begin(9600);
 }
 
+int seven_seg_val{0};
+bool a, b, c, d;
+
 void loop() {
+    a = seven_seg_val & 0x1;
+    b = seven_seg_val & 0x2;
+    c = seven_seg_val & 0x4;
+    d = seven_seg_val & 0x8;
+    digitalWrite(A, a);
+    digitalWrite(B, b);
+    digitalWrite(C, c);
+    digitalWrite(D, d);
+
+    seven_seg_val = seven_seg_val == 9 ? 0 : seven_seg_val+1;
+
+    Serial.print(seven_seg_val);
+    Serial.print(": ");
+    Serial.print(d);
+    Serial.print(c);
+    Serial.print(b);
+    Serial.println(a);
+    delay(500);
 }
